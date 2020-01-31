198f9f4d658563a4404ce6584b99a5f7fbce4862
Illuminubby <illuminubby@gmail.com>
2020-01-31 18:23:38 -0500

    updated dialogflow fulfillment code

--- Auxiliary Files/Dialogflow code files/index.js
+++ Auxiliary Files/Dialogflow code files/index.js
@@ -7,6 +7,7 @@ const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 const mysql = require('mysql');
+const dialogflow = require('dialogflow');
 
 var admin = require("firebase-admin");
 
@@ -15,6 +16,11 @@ admin.initializeApp();
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  
+  const etClient = new dialogflow.v2beta1.EntityTypesClient({
+  
+	});
+  
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
@@ -57,46 +63,18 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
       
   }
   
-    // Handles the content of the mysql query, using parameters given by the user
-    function queryDatabase (connection) {
-
-      // user-given unique identifier for a class
-      const userClassID = agent.request_.body.queryResult.outputContexts[0].parameters['ClassID.original'].toString();
-
-      // user-given question about a class
-      const userQuestion = agent.parameters.SYL_Question;
-
-      return new Promise((resolve, reject) => {
-
-      	// This query finds the answer to a question matching the user given value for a course with matching id
-        var sql = 'SELECT quesAns FROM ClassQuestions WHERE syllabusID = ' + userClassID;
-
-        //console.log(sql);
-
-        connection.query(sql, (error, results) => {
-
-        	// results will either be undefined if there is no existing question that meets the needs of the query
-            // or it will be the answer to a question
-            resolve(results);
-          
-        });
-        
-      });
-      
-  	}
-  
     // function to be used for custom professor questions
     /* NOT CURRENTLY IMPLEMENTED */
     function enterCustomQuery (connection) {
   	
       // user-given question about a class
-      const userQuestion = agent.request_.body.queryResult.outputContexts[0].parameters['SYL_Question.original'].toString();
+      const userQuestion = agent.request_.body.queryResult.outputContexts[0].parameters['ClassQuestion.original'];
 
       // user-given unique identifier for a class
-      const userClassID = agent.request_.body.queryResult.outputContexts[0].parameters['ClassID.original'].toString();
+      const userClassID = agent.request_.body.queryResult.outputContexts[0].parameters['ClassID.original'];
 
-      var sql = "SELECT quesID FROM ClassQuestions WHERE classID = " + userClassID;
-
+      var sql = 'SELECT atext FROM Question WHERE quesID in (SELECT quesID FROM ClassQuestions WHERE classID = ' + userClassID + ') AND qtext = \'' + userQuestion + '\'';
+		
       return new Promise ((resolve, reject) => {
 
           connection.query(sql, (error, results) => {
@@ -111,20 +89,20 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
       
   	}
   	
-  	// creates sql query for picking the answer to a question involving the syllabus
+  // creates sql query for picking the answer to a question involving the syllabus
 	function enterSyllabusQuery (connection, sylID) {
   	
       // user-given unique identifier for a class
-      const userClassID = agent.request_.body.queryResult.outputContexts[0].parameters['ClassID.original'].toString();
+      const userClassID = agent.request_.body.queryResult.outputContexts[0].parameters['ClassID.original'];
 
       // This query finds the answer to a question matching the user given value for a course with matching id
       // user-given question about a class
       const userQuestion = agent.request_.body.queryResult.outputContexts[0].parameters.SYL_Question;
-    
+    	
     	return new Promise((resolve, reject) => {
 
       		var sql = 'SELECT ' + userQuestion + ' FROM Syllabus WHERE syllabusID = ' + sylID;
-
+			
           	connection.query(sql, (error, results) => {
 
               	// results will either be undefined if there is no existing question that meets the criteria of the query
@@ -137,7 +115,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
       
   	}
   
-  	// creates sql query for retrieving syllabus id for a class
+  // creates sql query for retrieving syllabus id for a class
 	function getSyllabusForClass (connection) {
   	
       // user-given unique identifier for a class
@@ -161,85 +139,68 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   
   function handleReadFromMySQL (agent) {
 	
-    var syllabusIDn;
+	var syllabusIDn;
+	const userQuestion = agent.request_.body.queryResult.outputContexts[0].parameters.SYL_Question;
     
-    const userQuestionContent = agent.request_.body.queryResult.outputContexts[0].parameters.SYL_Question;
-    const userQuestionContentOriginal = agent.request_.body.queryResult.outputContexts[0].parameters['ClassID.original'].toString();
-    
-  	if (userQuestionContent != 'customInput') {
-    
-      return connectToDatabase().then(connection => {
+	return connectToDatabase().then(connection => {
+	
+		return getSyllabusForClass(connection).then(sID => {
+			
+			sID.map(ansr => {
+				
+				syllabusIDn = ansr.syllabusID;
+				
+			});
+			
+			return enterSyllabusQuery(connection, syllabusIDn).then(result => {
+				
+				result.map(ansr => {
+					//console.log(ansr);
+					if (ansr != undefined) {
+						//console.log(ansr.userQuestionContent);
+						agent.add(ansr[userQuestion]);
+						
+					}
+					else {
+						
+						agent.add("I'm sorry, there is no answer available for your question.");
+						
+					}
+					
+				});
+				
+				connection.end();
+				
+			});
+			
+		});
 		
-        return getSyllabusForClass(connection).then(sID => {
-        
-          sID.map(ansr => {
-            
-            syllabusIDn = ansr.syllabusID;
-            
-          });
-          
-          return enterSyllabusQuery(connection, syllabusIDn).then(result => {
+	});
+	
+}
 
-			  result.map(ansr => {
-                
-                if (ansr[userQuestionContent] != undefined) {
-                  
-                	agent.add(ansr[userQuestionContent]);
-                  
-                }
-                else {
-                  
-                	agent.add("I'm sorry, there is no answer available for your question.");
-                  
-                }
-                
-              });
-            
-              connection.end();
-
-          });
-          
-      	});
-        
-      });
-      
-  	}
-    
-    else {
-    	
-    	return connectToDatabase().then(connection => {
-          
-          return enterCustomQuery(connection).then(result => {
-
-			result.map(ansr => {
-				console.log(ansr);
-                
-                /*if (ansr[userQuestion]) {
-                	agent.add(ansr[userQuestion]);
-                }
-                else {
-                	agent.add("I'm sorry, there is no answer available for your question.");
-                }*/
-                
-              });
-            	connection.end();
-       
-          		});
-      	
-      		});
-    	}
-    
-  }
-
-  function handleCustomQuestion (agent) {
-  	console.log(agent.request_.body.queryResult);
-  }
+function handleCustomQuestion (agent) {
+	
+	const userQuestionContentOriginal = agent.request_.body.queryResult.outputContexts[0].parameters['classQuestion.original'];
+  
+	return connectToDatabase().then(connection => {
+		
+		return enterCustomQuery(connection).then(answer => {
+          	answer.map(ansr => {
+            	agent.add(ansr.atext);
+            });
+        });
+			
+	});
+	
+}
 
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('class_question', handleReadFromMySQL);
   intentMap.set('question_fallback', handleCustomQuestion);
+  intentMap.set('question_start', handleReadFromMySQL);
+  intentMap.set('custom_question', handleCustomQuestion);
   agent.handleRequest(intentMap);
 });
